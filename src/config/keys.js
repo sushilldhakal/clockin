aac8a5c6f9aeb42bfc87a45819676ca14ee599f9
Sushil Dhakal <sushildhakal1@gmail.com>
2021-12-08 11:38:47 +1100

    setup to post in heroku

---
+++ src/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://clock-in:vwvaR5YVffwzyrZo@testtravel.xcy06.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  secretOrKey: "secret"
+};
