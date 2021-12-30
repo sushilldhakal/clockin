26da4c603432928b70700ba8318d50f83fe56599
sushil <sus.hill.dhakal@gmail.com>
2021-12-30 23:15:10 +1100

    changes

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Card } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
+import { API_SERVER } from "../../config/constant";
 import Breadcrumb from "../../layouts/AdminLayout/Breadcrumb";
 
 import swal from "sweetalert";
@@ -29,7 +30,7 @@ export default class Login extends React.Component {
     const pwd = bcrypt.hashSync(this.state.password, salt);
 
     axios
-      .post(process.env.REACT_APP_BASE_URL + "auth/admin/login", {
+      .post(API_SERVER + "auth/admin/login", {
         username: this.state.username,
         password: this.state.password,
       })
@@ -62,54 +63,57 @@ export default class Login extends React.Component {
               <span className="r" />
             </div>
 
-            <form 
-                  onSubmit={this.login.bind(this)}>
-            <Card className="borderless text-center">
-              <Card.Body>
-                <div className="mb-4">
-                  <i className="feather icon-unlock auth-icon"></i>
-                </div>
-                <h3 className="mb-4">Login</h3>
+            <form onSubmit={this.login.bind(this)}>
+              <Card className="borderless text-center">
+                <Card.Body>
+                  <div className="mb-4">
+                    <i className="feather icon-unlock auth-icon"></i>
+                  </div>
+                  <h3 className="mb-4">Login</h3>
 
-                <TextField
-                  id="standard-basic"
-                  type="text"
-                  autoComplete="off"
-                  name="username"
-                  value={this.state.username}
-                  onChange={(e) => this.setState({ username: e.target.value })}
-                  placeholder="User Name"
-                  required
-                />
-                <br />
-                <br />
-                <TextField
-                  id="standard-basic"
-                  type="password"
-                  autoComplete="off"
-                  name="password"
-                  value={this.state.password}
-                  onChange={(e) => this.setState({ password: e.target.value })}
-                  placeholder="Password"
-                  required
-                />
-                <br />
-                <br />
-                <Button
-                  className="button_style"
-                  variant="contained"
-                  color="primary"
-                  size="small"
-                  type="submit"
-                  disabled={
-                    this.state.username == "" && this.state.password == ""
-                  }
-                  onClick={this.login.bind(this)}
-                >
-                  Login
-                </Button>
-              </Card.Body>
-            </Card>
+                  <TextField
+                    id="standard-basic"
+                    type="text"
+                    autoComplete="off"
+                    name="username"
+                    value={this.state.username}
+                    onChange={(e) =>
+                      this.setState({ username: e.target.value })
+                    }
+                    placeholder="User Name"
+                    required
+                  />
+                  <br />
+                  <br />
+                  <TextField
+                    id="standard-basic"
+                    type="password"
+                    autoComplete="off"
+                    name="password"
+                    value={this.state.password}
+                    onChange={(e) =>
+                      this.setState({ password: e.target.value })
+                    }
+                    placeholder="Password"
+                    required
+                  />
+                  <br />
+                  <br />
+                  <Button
+                    className="button_style"
+                    variant="contained"
+                    color="primary"
+                    size="small"
+                    type="submit"
+                    disabled={
+                      this.state.username == "" && this.state.password == ""
+                    }
+                    onClick={this.login.bind(this)}
+                  >
+                    Login
+                  </Button>
+                </Card.Body>
+              </Card>
             </form>
           </div>
         </div>
