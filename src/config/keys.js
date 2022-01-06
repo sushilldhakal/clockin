4094ed7186ca1fb0c895f8f4eab23c5c1f0c2b01
sushilldhakal <sus.hill.dhakal@gmail.com>
2022-01-06 23:22:53 +1100

    server changes

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongoURI:
     process.env.mongoURI ||
-    "mongodb+srv://clock-in:2yQcaolU1cN8lqzq@cluster0.chm3w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "mongodb+srv://clock-in:admin@cluster0.chm3w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   secretOrKey: "secret"
 };
--- src/controllers/delete_employee.js
+++ src/controllers/delete_employee.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-const connect = require("../config/connect");
-const { ObjectId } = require("bson");
-const moment = require("moment");
-
-module.exports = async (request, reply) => {
-  const client = await connect();
-  const db = client.db("clock-in-users");
-  const collection = db.collection("employees");
-
-  // delete empoloyee
-  const id = request.params.employee_id;
-
-  const employee = await db
-    .collection("categories")
-    .findOne({ _id: ObjectId(id) });
-
-  if (employee) {
-    const remove = await db
-      .collection("employee")
-      .deleteOne({ _id: ObjectId(id) });
-
-    client.close();
-
-    if (remove) {
-      return reply.send({
-        success: true,
-        message: "Staff deleted successfully",
-        employee,
-      });
-    }
-  }
-};
-=======
 const connect = require("../config/connect");
 const { ObjectId } = require("bson");
 const moment = require("moment");
@@ -60,4 +26,3 @@ module.exports = async (request, reply) => {
     message: "Employee deleted successfully"
   });
 };
->>>>>>> 6e28a57e2555b697555ceef2ab57dce7657376aa
