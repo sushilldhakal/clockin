9066ea90c7dd8d6e58b6bf612e703b7624709310
sushil <sus.hill.dhakal@gmail.com>
2022-01-12 02:01:17 +1100

    new changes

---
+++ client/src/views/Flag/Flag.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Flag() {
+  return (
+    <div className="timesheet-body-page">
+      <div className="dashboard-body">Flag page</div>
+    </div>
+  );
+}
