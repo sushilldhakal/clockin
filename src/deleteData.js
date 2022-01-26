42460f46136f3491905993a792fa1f4e3eeb290e
Suman Shrestha <summonshr@gmail.com>
2022-01-26 15:05:27 +0530

    WIP

--- src/deleteData.js
+++ src/deleteData.js
@@ -1,25 +1,38 @@
 const { MongoClient } = require("mongodb");
-const moment = require("moment");
+const moment = require('moment')
 const uri = `mongodb+srv://clock-in:vwvaR5YVffwzyrZo@testtravel.xcy06.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-client.connect((err, client) => {
-  const db = client.db("clock-in-users");
-  const collection = db.collection("timesheets");
 
-  if (err) throw err;
-  var myquery = {
-    date: {
-      $lte: "15-01-2022",
-    },
-
-    //date: "15-01-2022",
-  };
-  db.collection("timesheets").deleteMany(myquery, function (err, obj) {
-    if (err) throw err;
-    console.log(obj.result + " document(s) deleted");
-    client.close();
+async function deleteData() {
+  const mongo = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   });
-});
+
+  try {
+    await mongo.connect();
+
+    const db = mongo.db("clock-in-users");
+
+    const collection = db.collection("timesheets");
+
+    // fetch all timesheets
+    const timesheets = await collection.find({
+    }, { date: 1, _id: 1 }).toArray();
+
+    let deletable = timesheets.filter(timesheet => {
+      return moment(timesheet.date,'DD-MM-YYYY').isBefore(moment(), 'day')
+    }).map(timesheet => {
+      return collection.deleteOne({ _id: timesheet._id });
+    })
+
+    await Promise.all(deletable);
+
+    await mongo.close();
+
+
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+deleteData()
