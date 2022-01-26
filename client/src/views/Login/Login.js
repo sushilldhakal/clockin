7a43b952f0ca39e9ba52860eb5c58ddc0ed63d4e
Suman Shrestha <summonshr@gmail.com>
2022-01-26 14:02:29 +0530

    WIP

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -37,7 +37,8 @@ export default class Login extends React.Component {
       .then((res) => {
         localStorage.setItem("token", res.data.token);
         localStorage.setItem("user_id", res.data.id);
-        localStorage.setItem("location", res.data.location);
+        if (Boolean(res.data.location))
+          localStorage.setItem("location", res.data.location);
         this.props.history.push("/dashboard");
       })
       .catch((err) => {
