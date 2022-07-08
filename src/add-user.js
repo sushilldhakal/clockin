f5480348fbfc817943e0a444e9151afcf5bc9d55
Sushil Dhakal <sus.hill.dhakal@gmail.com>
2022-07-08 12:15:15 +1000

    Update add-user.js

--- src/add-user.js
+++ src/add-user.js
@@ -6,34 +6,8 @@ connect()
       .db("clock-in-users")
       .collection("users")
       .insertOne({
-        username: "portmelbourne",
-        password: bcrypt.hashSync("portmelbourne@123", 10),
-        location: "Port Melbourne",
-      });
-
-    client
-      .db("clock-in-users")
-      .collection("users")
-      .insertOne({
-        username: "dandenong",
-        password: bcrypt.hashSync("dandenong@123", 10),
-        location: "Dandenong",
-      });
-    client
-      .db("clock-in-users")
-      .collection("users")
-      .insertOne({
-        username: "tullamarine",
-        password: bcrypt.hashSync("tullamarine@123", 10),
-        location: "Tullamarine",
-      });
-
-    client
-      .db("clock-in-users")
-      .collection("users")
-      .insertOne({
         username: "admin",
-        password: bcrypt.hashSync("admin@123", 10),
+        password: bcrypt.hashSync("admin", 10),
       });
     //client.close();
   })
