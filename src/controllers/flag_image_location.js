ab39340f7f1b9600d994dc77ec11a2f7e2bd3e3e
sushil <sus.hill.dhakal@gmail.com>
2022-07-10 19:37:59 +1000

    changes

---
+++ src/controllers/flag_image_location.js
@@ -0,0 +1,47 @@
+const connect = require("../config/connect");
+const moment = require("moment");
+
+module.exports = async (request, reply) => {
+  const client = await connect();
+  const db = client.db("clock-in-users");
+  const collection = db.collection("timesheets");
+  const users = await db.collection("employees").find({}).toArray();
+
+  const timesheets = await collection
+    .find({
+      pin: { $in: users.map((e) => e.pin) },
+      image: null,
+      lat: null,
+      lng: null,
+    })
+    .map(({ type, date, time, pin, image, where }) => {
+      let user = users.filter((user) => user.pin === pin)[0];
+      if (time) {
+        time = moment(time).format("h:mm a");
+      }
+      if (user) {
+        let { name, role, hire, site, _id } = user;
+        return {
+          _id,
+          type,
+          date,
+          time,
+          name,
+          pin,
+          role,
+          hire,
+          site,
+          image,
+          where,
+        };
+      }
+      return { type, date, time, pin, image, where };
+    })
+    .toArray();
+
+  client.close();
+  reply.send({
+    timesheets,
+    message: "Timesheets fetched successfully 1",
+  });
+};
