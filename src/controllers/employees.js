30c12cc96ab92eb9210e5bbdad62bb498548c0b4
sushil <sus.hill.dhakal@gmail.com>
2022-07-11 23:10:32 +1000

    changes

--- src/controllers/employees.js
+++ src/controllers/employees.js
@@ -1,17 +1,17 @@
-const connect = require("../config/connect")
-const jwt = require('jsonwebtoken')
+const connect = require("../config/connect");
+const jwt = require("jsonwebtoken");
 
 module.exports = async (request, reply) => {
   const client = await connect();
   const db = client.db("clock-in-users");
 
-  const token = jwt.decode(request.headers.api_key)
+  const token = jwt.decode(request.headers.api_key);
   const collection = db.collection("employees");
   let employees = await collection.find({}).toArray();
-  if (token && token.id === 'payable') {
-    employees = employees.filter(r => {
-      return r.hire !== 'Employee' && r.hire !== 'Employees'
-    })
+  if (token && token.id === "payable") {
+    employees = employees.filter((r) => {
+      return r.hire !== "Employees";
+    });
   }
   await client.close();
   reply.send(employees);
